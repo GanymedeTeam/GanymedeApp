0e0ef3088b9d35738710ac8119bbe9600442b175
Massart Benjamin <benjamin.pynx.massart@gmail.com>
2024-05-28 11:14:53 +0200

    Package files
    Rajout des fichiers du package AppWindowsUtility dans le projet

---
+++ Assets/Packages/AppWindowUtility-main/Runtime/IPlatformDependent.cs
@@ -0,0 +1,40 @@
+﻿namespace SatorImaging.AppWindowUtility
+{
+    public interface IPlatformDependent
+    {
+        bool AlwaysOnTopSupported { get; }
+        bool GetAlwaysOnTop();
+        void SetAlwaysOnTop(bool enable);
+
+        bool TransparentSupported { get; }
+        bool GetTransparent();
+        void SetTransparent(bool enable);
+        void SetScreenSize(int sizeX, int sizeY);
+
+        bool FrameVisibilitySupported { get; }
+        bool GetFrameVisibility();
+        void SetFrameVisibility(bool visible);
+
+        bool ClickThroughSupported { get; }
+        bool GetClickThrough();
+        void SetClickThrough(bool enable);
+
+        bool AsWallpaperSupported { get; }
+        bool GetAsWallpaper();
+        void SetAsWallpaper(bool enable);
+
+
+
+        bool KeyingColorSupported { get; }
+        void SetKeyingColor(byte red, byte green, byte blue);
+
+        bool WindowOpacitySupported { get; }
+        void SetWindowOpacity(byte opacity);
+
+
+        bool WindowPlacementSupported { get; }
+        void MoveWindowRelative(int pixelX, int pixelY);
+
+    }//
+
+}//namespace
