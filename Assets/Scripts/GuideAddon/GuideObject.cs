cf60473d9163c32b5958435c97fb1f24927b6729
pray-mess <paul.ray@hotmail.fr>
2024-05-29 23:33:42 +0200

    search_directories_opendirectoryguides

--- Assets/Scripts/GuideAddon/GuideObject.cs
+++ Assets/Scripts/GuideAddon/GuideObject.cs
@@ -2,23 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class GuideObject : MonoBehaviour
 {
     public TMP_Text guideNameText;
     public string guideName;
-    
-    public void Initialize(string guideName)
+    private string directoryPath;
+    private bool isDirectory;
+    public Image guideIcon;
+
+    public void Initialize(string guideName, bool isDirectory, string directoryPath = null)
     {
         this.guideName = guideName;
+        this.directoryPath = directoryPath;
+        this.isDirectory = isDirectory;
         guideNameText.text = guideName;
+
+        if (isDirectory)
+        {
+            guideIcon.enabled = true;
+        }
+        else
+        {
+            guideIcon.enabled = false;
+        }
     }
 
     public void OpenGuide()
     {
         GuideMenu guideMenu = FindObjectOfType<GuideMenu>();
-        guideMenu.GuideDetailsMenu.SetActive(true);
-        guideMenu.GuideSelectionMenu.SetActive(false);
-        guideMenu.LoadGuide(guideName);
+
+        if (isDirectory)
+        {
+            guideMenu.ReloadGuideList(directoryPath);
+        }
+        else
+        {
+            guideMenu.GuideDetailsMenu.SetActive(true);
+            guideMenu.GuideSelectionMenu.SetActive(false);
+            guideMenu.LoadGuide(guideName);
+        }
     }
 }
