9380e60b7ec9af452057c33d66b3ce72d6d57466
mouahahaaa <zehahahaaa@gmail.com>
2024-07-28 19:27:54 +0200

    reload map only when entering guide content

--- Assets/Scripts/GuideMenuUtilities/GuideDetailsMenu.cs
+++ Assets/Scripts/GuideMenuUtilities/GuideDetailsMenu.cs
@@ -3,17 +3,19 @@ using UnityEngine;
 public class GuideDetailsMenu : MonoBehaviour
 {
     public GameObject guideMenu;
-    public GameObject mapManager;
-    public GameObject mapMask;
     private GuideMenu guideMenuScript;
+    public GameObject windowManager;
+    private WindowManager windowManagerScript;
 
-    void Start()
+    void Awake()
     {
         guideMenuScript = guideMenu.GetComponent<GuideMenu>();
+        windowManagerScript = windowManager.GetComponent<WindowManager>();
     }
 
     void OnEnable()
     {
-        guideMenu.GetComponent<GuideMenu>().LoadGuide(guideMenu.GetComponent<GuideMenu>().OpenedGuide);
+        windowManagerScript.InGuideRefreshInteractiveMap();
+        guideMenuScript.LoadGuide(guideMenu.GetComponent<GuideMenu>().OpenedGuide);
     }
 }
