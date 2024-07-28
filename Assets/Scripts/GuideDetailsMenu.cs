50d25797f03214ff18fc047a165b0a3067b0235c
mouahahaaa <zehahahaaa@gmail.com>
2024-07-28 17:56:21 +0200

    fix guide details error when loading

--- Assets/Scripts/GuideDetailsMenu.cs
+++ Assets/Scripts/GuideDetailsMenu.cs
@@ -2,11 +2,10 @@ using UnityEngine;
 
 public class GuideDetailsMenu : MonoBehaviour
 {
-    public GameObject winManager;
     public GameObject guideMenu;
 
     void OnEnable()
     {
-        guideMenu.GetComponent<GuideMenu>().GoToGuideStep(guideMenu.GetComponent<GuideMenu>().guideProgress);
+        guideMenu.GetComponent<GuideMenu>().LoadGuide(guideMenu.GetComponent<GuideMenu>().OpenedGuide);
     }
 }
